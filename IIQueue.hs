f0f4e9f1b1f799cbda0e3a0be13ed41ef4d50e66
Tinco Andringa <mail@tinco.nl>
2014-01-05 23:33:37 +0100

    work out more of the connector system

--- IIQueue.hs
+++ IIQueue.hs
@@ -25,6 +25,7 @@ data QueueState = QueueState {
 
 data StorageType = Disk | DiskMemory
 
+data Configuration = Configuration {}
 
 {-
 
@@ -46,8 +47,6 @@ A single thread connects readers to writers.
 
 -}
 
-data Configuration = Configuration {}
-
 main :: IO ()
 main = do
 	configuration <- readConfiguration
@@ -68,7 +67,7 @@ data ConnectorS = ConnectorS {
 }
 
 startConnector :: Configuration -> TChan(Socket) -> TChan(Socket) -> IO()
-startConnector c wsC rsC = connectorLoop $ newQueueState c
+startConnector c wsC rsC = connectorLoop $ newConnectorS c
 	where
 		connectorLoop cs = do
 			ws' <- atomically $ appendChanToList wsC ws
@@ -90,23 +89,93 @@ startConnector c wsC rsC = connectorLoop $ newQueueState c
 					newVal <- readTChan chan
 					return $ newVal : list
 
+{-
+
+So the goal is to get messages from readers to writers.
+
+When there is a reader:
+	The reader will read the oldest message, whether it is on disk or in memory.
+
+	When the oldest message is on disk but the disk is not available
+	the oldest message in memory is read instead.
+
+	If the memory is not available either, the reader is connected
+	to a writer instead.
+
+Whenever there is a writer with a message, but no reader
+ready for it messages are buffered to memory and to disk.
+
+Note: This system is fair, but most performant when there are
+more than 2 readers, as the first reader is reading slowly
+from disk, and the second is reading from memory. Any additional
+readers will read directly from writers.
+Note: The previous note only happens when at some point readers are
+slower than writers.
+
+-}
 connectResources :: ConnectorS -> IO(ConnectorS)
-connectResources cs 
-	| ws == [] && rs == [] = return cs
-	| ws == [] = handleJustReaders cs
-	| otherwise = do
-		connectWriterToReader w r
-		connectResources restCS
+connectResources cs@(ConnectorS ws rs _)
+	-- TODO would this be faster if we iterated over all readers
+	-- at once? Using the monadic fold thingy.
+	| not $ null rs = connectReader cs 
+	| not $ null ws = bufferWriter cs
+	| otherwise = return cs
+
+{- Connects a reader to a message. -}
+connectReader :: ConnectorS -> IO(ConnectorS)
+connectReader cs@(ConnectorS ws _ qs)
+	| oldestMessageAvailable = readOldestMessage cs
+	| memoryHasAMessage = readOldestMemoryMessage cs
+	| writerAvailable = connectToWriter cs
+	| otherwise = return cs
 	where
-		ws@[w:rws] = cnWriters cs
-		rs@[r:rrs] = cnReaders cs
-		qs = cnQueueState cs
-		restCS = cs { cnWriters = rws, cnReaders = rrs}
+		oldestMessageAvailable = undefined qs
+		memoryHasAMessage = undefined qs
+		writerAvailable = not $ null ws
 
+{- Persists a message from a writer. -}
+bufferWriter :: ConnectorS -> IO(ConnectorS)
+bufferWriter cs = undefined cs
 
-startWritersListener = undefined
+{-
+ Starts listening for writers, puts something on the TChan when it's ready
+ for message production.
+-}
+startWritersListener :: Configuration -> IO(TChan Socket)
+startWritersListener c = undefined c
+
+{-
+ Starts listening for readers, puts the reader on the TChan when it's ready
+ for message consumption.
+-}
+startReadersListener :: Configuration -> IO(TChan Socket)
 startReadersListener = undefined
-newQueueState c = undefined
+
+newQueueState :: Configuration -> QueueState
+newQueueState c = undefined c
+
+newConnectorS :: Configuration -> ConnectorS
+newConnectorS c = ConnectorS [] [] $ newQueueState c
+
+{-
+ Reads the oldest message, from disk or from memory. It is passed to the
+ first reader in the ConnectorS. 
+-}
+readOldestMessage :: ConnectorS -> IO(ConnectorS)
+readOldestMessage = undefined
+
+{-
+ Reads the oldest message in memory. It is passed to the
+ first reader in the ConnectorS. 
+-}
+readOldestMemoryMessage :: ConnectorS -> IO(ConnectorS)
+readOldestMemoryMessage = undefined
+
+{-
+ Connects the first reader in ConnectorS to the first writer. 
+-}
+connectToWriter :: ConnectorS -> IO(ConnectorS)
+connectToWriter = undefined
 
 {-}
 startWritersListener :: Configuration -> Context -> IO()
