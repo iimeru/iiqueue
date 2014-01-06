4d50ffe2de226de18661fda951cc04e9e1f442e2
Tinco Andringa <mail@tinco.nl>
2014-01-07 00:08:56 +0100

    think about queuestate a bit

--- QueueState.hs
+++ QueueState.hs
@@ -5,17 +5,31 @@ import Configuration
 import Data.ByteString as BS
 
 {-
+
 Things we want to know about a QueueState:
 
 oldestMessageAvailable,	memoryHasAMessage
+
+Basically we need to know about the oldest
+messages both on disk and in memory.
+
+For the disk the oldest message is where the
+pointer is right now. The should be something 
+that communicates whether the handle is
+available.
+
+For the memory, the oldest message could be
+the head of a list.
+
+Do we need to know whether the oldest message
+on disk is older than the oldest message in 
+memory?
 
 Things we want to do with a QueueState:
 
 readMessageFromDisk, readMessageFromMemory
 
 -}
-
-
 
 data QueueState = QueueState {
 	qsBufferUsed :: Int,
@@ -25,8 +39,14 @@ data QueueState = QueueState {
 	qsMaxMessageSize :: Int
 }
 
+mega :: Int -> Int
+mega = (*) (1024 * 1024)
+
+giga :: Int -> Int
+giga = (*) (mega 1024)
+
 newQueueState :: Configuration -> QueueState
-newQueueState c = undefined c
+newQueueState _ = QueueState 0 (mega 150) 0 (giga 100) (mega 100)
 
 data Message = Message Int Socket
 
